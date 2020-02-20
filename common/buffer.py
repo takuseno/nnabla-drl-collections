09000031020cff05dae2034d019b14df4fcec59d
takuseno <takuma.seno@gmail.com>
2020-02-20 23:59:28 +0900

    Improve replay buffer sample performance

--- common/buffer.py
+++ common/buffer.py
@@ -1,6 +1,35 @@
+import numpy as np
 import random
 
 from collections import deque
+
+
+# https://github.com/chainer/chainerrl/blob/master/chainerrl/misc/random.py
+def sample_n_k(n, k):
+    """Sample k distinct elements uniformly from range(n)"""
+
+    if not 0 <= k <= n:
+        raise ValueError("Sample larger than population or is negative")
+    if k == 0:
+        return np.empty((0,), dtype=np.int64)
+    elif 3 * k >= n:
+        return np.random.choice(n, k, replace=False)
+    else:
+        result = np.random.choice(n, 2 * k)
+        selected = set()
+        selected_add = selected.add
+        j = k
+        for i in range(k):
+            x = result[i]
+            while x in selected:
+                x = result[i] = result[j]
+                j += 1
+                if j == 2 * k:
+                    # This is slow, but it rarely happens.
+                    result[k:] = np.random.choice(n, k)
+                    j = k
+            selected_add(x)
+        return result[:k]
 
 
 class ReplayBuffer:
@@ -15,7 +44,10 @@ class ReplayBuffer:
         self.buffer.append(experience)
 
     def sample(self):
-        return random.sample(self.buffer, self.batch_size)
+        samples = []
+        for index in sample_n_k(self.size(), self.batch_size):
+            samples.append(self.buffer[index])
+        return samples
 
     def size(self):
         return len(self.buffer)
